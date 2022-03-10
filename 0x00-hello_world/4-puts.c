b093a8650863a7d934f3800ee5e120b3613ddaca
EdwardMungai1 <edumungai08@gmail.com>
2022-03-10 15:01:39 -0800

    Prints msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Outputs a string
+ * Return: 0 returned to exit the program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
