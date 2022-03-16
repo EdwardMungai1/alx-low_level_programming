efac01849a28efa019ef5cb8022e9b93170b31ee
EdwardMungai1 <edumungai08@gmail.com>
2022-03-15 19:22:57 -0700

    Print purchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#include "main.h"
+  2
+  3 /**
+  4  * main -prints putchar followed by a new line.
+  5 *
+  6 *Return: Always 0.
+  7 */
+  8 int main(void)
+  9 }
+ 10         _putchar('_');
+ 11         _putchar('p');
+ 12         _putchar('u');
+ 13         _putchar('t');
+ 14         _putchar('c');
+ 15         _putchar('h');
+ 16         _putchar('a');
+ 17         _putchar('r');
+ 18         _putchar('\n');
+ 19
+ 20         return (0);
+ 21  }
+ 22
+
